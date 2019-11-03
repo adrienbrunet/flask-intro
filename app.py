2e5eaff755f76c897e3a5229808cf0c615d7e27e
Adrien Brunet <adrien@mindee.co>
2019-11-03 18:15:52 +0100

    new: :sparkles: First views

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>ECM Bonjour</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,17 @@ from flask import Flask
 def test_app():
     assert app is not None
     assert isinstance(app, Flask)
+
+
+def test_index_route():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "ECM" in response.data.decode("utf-8")
+
+
+def test_index_user():
+    client = app.test_client()
+    response = client.get("/user/adrien")
+    assert response.status_code == 200
+    assert "Hello, adrien" in response.data.decode("utf-8")
